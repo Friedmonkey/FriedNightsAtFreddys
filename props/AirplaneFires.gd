b7818466e8fd5d3898d547fbe56c4fc311ebae3c
FriedMonkey <Martijn.Kraats5@gmail.com>
2026-02-22 14:39:23 +0100

    added camera shake

--- props/AirplaneFires.gd
+++ props/AirplaneFires.gd
@@ -5,18 +5,30 @@ var player : Node3D
 @onready var fire1 := $AirplaneFire
 @onready var fire2 := $AirplaneFire2
 
+@onready var left_bang := $LeftBang
+@onready var right_bang := $RightBang
+@onready var explosionTrauma := $ExplosionTrauma
+
+@onready var traumaCauser := $TraumaCauser
+
 @onready var dangerAlarm := $DangerAlarm
 @onready var beeping := $Beeping
 @onready var turbulance := $Turbulance
 @onready var siren := $Siren
 
 @onready var lights := $lights
+var has_calamity := false
 
 #var current_intensity: int = 0
 func _ready() -> void:
 	player = $"..".player
 	if player == null:
 		player.THE_PLAYER_IS_NULL("PLEASE ASSIGN A PLAYER IN THE EDITOR")
+
+func _process(_delta: float) -> void:
+	if has_calamity:
+		traumaCauser.cause_trauma()
+		#pass
 
 #debug stuff
 func _input(event: InputEvent) -> void:
@@ -27,16 +39,18 @@ func _input(event: InputEvent) -> void:
 		set_calamity(false)
 
 func set_calamity(enabled: bool):
+	has_calamity = enabled
 	var intensity = 5 if enabled else 0
 	fire1.set_intensity(intensity)
 	fire2.set_intensity(intensity)
 	
-	#todo: add camera shake system
-	#https://www.youtube.com/watch?v=1i5SB8Ct1y0
-	#player.set_camera_shake(enabled)
 	#maby also play an explosion sound when setting it to true?
 	
 	if (enabled):
+		left_bang.play()
+		right_bang.play()
+		explosionTrauma.cause_trauma()
+		
 		set_lights(Color.DARK_RED)
 		dangerAlarm.play()
 		beeping.play()
