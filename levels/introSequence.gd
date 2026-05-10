8c16b9d156c95d1f82525bed9c020885d3ec2233
FriedMonkey <Martijn.Kraats5@gmail.com>
2026-05-10 20:24:25 +0200

    fixed intro audio not syncing

--- levels/introSequence.gd
+++ levels/introSequence.gd
@@ -3,15 +3,33 @@ extends Node2D
 
 #var intro_scene := preload("res://levels/woods.scn")
 const game_scene := preload("res://levels/woods.scn")
+@onready var music: AudioStreamPlayer = $AudioStreamPlayer2D
+
+const SYNC_OFFSET := 0.02
+const SYNC_SPEED := 10.0
+
+func _process(delta: float) -> void:
+	if music.playing:
+		var target_pos = music.get_playback_position() + SYNC_OFFSET
+
+		# smooth correction
+		var current_pos = sequence_player.current_animation_position
+		var synced_pos = lerp(current_pos, target_pos, delta * SYNC_SPEED)
+
+		sequence_player.seek(synced_pos, true)
 
 func start(player: Node3D):
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	sequence_player.play("intro_2")
+	music.play()
 	await player.Transition("fade_out");
 	#var gameplay = game_scene.instantiate()
 	#get_tree().root.add_child(gameplay)
 	get_tree().change_scene_to_packed(game_scene)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	await get_tree().process_frame
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	#player.Transition("fade_in");
 #	thje player has to get loaded first??? because player hasnt loaded its vars yet here?
 	var new_scene = get_tree().current_scene
